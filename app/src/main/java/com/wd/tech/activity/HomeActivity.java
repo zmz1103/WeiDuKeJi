c60716daf03637065c013defda71bafbd7714433
zmz1103 <45384595+zmz1103@users.noreply.github.com>
2019-02-19 11:11:40 +0800

    yilai

---
+++ app/src/main/java/com/wd/tech/activity/HomeActivity.java
@@ -0,0 +1,25 @@
+package com.wd.tech.activity;
+
+import com.wd.tech.R;
+
+/**
+ * date:2019/2/19 10:52
+ * author:赵明珠(啊哈)
+ * function:
+ */
+public class HomeActivity extends WDActivity{
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_home;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void destoryData() {
+
+    }
+}
