1fc87347050f69e32389e1e99a8496b913ffa34a
淡意衬优柔 <lk18833433822@163.com>
2019-02-19 20:38:51 +0800

    资讯页面

--- app/src/main/java/com/wd/tech/activity/MainActivity.java
+++ app/src/main/java/com/wd/tech/activity/MainActivity.java
@@ -1,16 +1,32 @@
 package com.wd.tech.activity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
 import com.wd.tech.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.ceshi)
+    TextView ceshi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @OnClick(R.id.ceshi)
+    public void onViewClicked() {
+        startActivity(new Intent(MainActivity.this,HomeActivity.class));
 
     }
 }
--- app/src/main/java/com/wd/tech/https/IRequest.java
+++ app/src/main/java/com/wd/tech/https/IRequest.java
@@ -1,9 +1,40 @@
 package com.wd.tech.https;
 
+import com.wd.tech.bean.BannnerBean;
+import com.wd.tech.bean.InformationListBean;
+import com.wd.tech.bean.Result;
+
+import java.util.List;
+
+import io.reactivex.Observable;
+import okhttp3.MultipartBody;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+
 /**
  * date:2019/2/18 20:28
  * author:赵明珠(啊哈)
  * function:
  */
 public interface IRequest {
+
+    /**
+     * Banner轮播图
+     * @return
+     */
+    @GET("information/v1/bannerShow")
+    Observable<Result<List<BannnerBean>>> ShowBannner();
+
+    @POST("information/v1/infoRecommendList")
+    @FormUrlEncoded
+    Observable<Result<List<InformationListBean>>> showinformationList(@Header("userId") int userId,
+                                                                      @Header("sessionId")String sessionId,
+                                                                      @Field("plateId")int plateId,
+                                                                      @Field("page")int page,
+                                                                      @Field("count")int count);
+
 }
