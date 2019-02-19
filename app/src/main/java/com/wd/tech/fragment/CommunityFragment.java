acbb67b5051c12b61492e2bd8fdf88c10636ebbd
zmz1103 <45384595+zmz1103@users.noreply.github.com>
2019-02-19 15:47:56 +0800

    fragment

---
+++ app/src/main/java/com/wd/tech/fragment/CommunityFragment.java
@@ -0,0 +1,22 @@
+package com.wd.tech.fragment;
+
+import android.view.View;
+
+import com.wd.tech.R;
+
+/**
+ * date:2019/2/19 13:53
+ * author:赵明珠(啊哈)
+ * function:
+ */
+public class CommunityFragment extends WDFragment{
+    @Override
+    public int getContent() {
+        return R.layout.activity_community_fragment;
+    }
+
+    @Override
+    public void initView(View view) {
+
+    }
+}
