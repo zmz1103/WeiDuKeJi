0da528d09a395a96f67b4034781386a0e5c83d98
zmz1103 <45384595+zmz1103@users.noreply.github.com>
2019-03-08 08:48:08 +0800

    啊哈

--- app/src/main/java/com/wd/tech/bean/FindConversationList.java
+++ app/src/main/java/com/wd/tech/bean/FindConversationList.java
@@ -1,16 +1,38 @@
 package com.wd.tech.bean;
 
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Generated;
+
 /**
  * date:2019/3/6 20:37
  * author:赵明珠(啊哈)
  * function:
  */
+@Entity
 public class FindConversationList {
+    @Id
+    private long id;
     private String headPic;
     private String nickName;
     private String pwd;
     private int userId;
     private String userName;
+
+    @Generated(hash = 998582365)
+    public FindConversationList(long id, String headPic, String nickName,
+            String pwd, int userId, String userName) {
+        this.id = id;
+        this.headPic = headPic;
+        this.nickName = nickName;
+        this.pwd = pwd;
+        this.userId = userId;
+        this.userName = userName;
+    }
+
+    @Generated(hash = 1653457)
+    public FindConversationList() {
+    }
 
     public String getHeadPic() {
         return headPic;
@@ -50,5 +72,17 @@ public class FindConversationList {
 
     public void setUserName(String userName) {
         this.userName = userName;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
